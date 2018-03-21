d3d284ba3e454fbe2ef1a2ac34c8a6c90954da14
clay53 <clayton.hickey@yahoo.com>
2018-03-21 00:12:49 -0400

    Added species

--- README.md
+++ README.md
@@ -12,3 +12,28 @@ const keys = {
 
 export default keys
 ```
+
+# ex database
+
+```
+{
+  "species" : {
+    "-L6xI7vVUTRvECo7Meu2" : {
+      "name" : "test",
+      "spottingCount" : 2
+    }
+  },
+  "spottings" : {
+    "-L6xI7vVUTRvECo7Meu1" : {
+      "confirmed" : true,
+      "image" : "https://firebasestorage.googleapis.com/v0/b/featheredfriends-1e2dd.appspot.com/o/images%2F1520373106.16.jpg?alt=media",
+      "time" : "1520373106.16"
+    },
+    "-L6xIG_wFpGg82d-T6Yi" : {
+      "confirmed" : false,
+      "image" : "https://firebasestorage.googleapis.com/v0/b/featheredfriends-1e2dd.appspot.com/o/images%2F1520373141.35.jpg?alt=media",
+      "time" : "1520373141.35"
+    }
+  }
+}
+```
--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -14,18 +14,17 @@ class Dashboard extends Component {
     }
     componentDidMount () {
         db.ref('spottings').on('value', snapshot => {
-            console.log(snapshot.val())
             var results = [];
             snapshot.forEach(spotting => {
                 if (spotting.child('confirmed').val() !== true) {
                     results.push(spotting.val());
                 }
             });
-            console.log(results);
             this.setState({
                 spottings: results
             });
         });
+        
     }
     render() {
         return (
@@ -42,13 +41,81 @@ class Dashboard extends Component {
 }
 
 class Spotting extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "",
+            search: "Search Species/New",
+            species: {}
+        };
+        
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleConfirm = this.handleConfirm.bind(this);
+    }
+    
+    componentDidMount () {
+        db.ref('species').on('value', snapshot => {
+            console.log(snapshot.val());
+            this.setState({
+                species: snapshot.val()
+            })
+        });
+    }
+
+    handleSearch(event) {
+        console.log("Search query changed");
+        this.setState({search: event.target.value});
+        this.setState({value: "New"})
+    }
+
+    handleSelect(event) {
+        console.log("Select area has changed");
+        console.log(event.target.value);
+        this.setState({value: event.target.value});
+    }
+
+    handleConfirm(event) {
+        console.log("Confirming...");
+        if (this.state.value === "") {
+            console.log("No value!");
+        } else if (this.state.value === "New") {
+            console.log("Created new species:");
+            db.ref('species/').push().set({
+                'name': this.state.search,
+                'spottingCount': 1
+            })
+        } else {
+            db.ref('species/' + this.state.value).once('value', snapshot => {
+                console.log("Adding one to " + this.state.value);
+                snapshot.ref.child('spottingCount').set(snapshot.child('spottingCount').val()+1);
+            })
+        }
+    }
+
     render() {
         return (
             <div>
                 <img src={this.props.spotting.image} alt="img of bird" width="60%"/>
                 <br/>
-                <button>Confirm</button>
-                <a></a>
+                <button onClick={this.handleConfirm}>Confirm</button>
+                <a>key: {this.state.value}</a>
+                <br/>
+                <input value={this.state.search} onChange={this.handleSearch}></input>
+                <select onChange={this.handleSelect}>
+                    <option value="New"></option>
+                    {
+                        // eslint-disable-next-line
+                        Object.entries(this.state.species).map((species) =>
+                            {
+                                if (species[1].name.toLowerCase().startsWith(this.state.search.toLowerCase()) ||
+                                this.state.search === "Search Species/New") {
+                                    return(<option value={species[0]} key={species[0]}>{species[1].name}</option>);
+                                }
+                            }
+                        )
+                    }
+                </select>
             </div>
         )
     }
